035b02272393f59105d25b5baf3b03d42036e2d6
izunna1026 <done4emma@yahoo.com>
2023-11-30 19:51:22 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    k = 0
-    
-    for l in range(len(sys.argv) - 1):
-        k += int(sys.argv[l + 1])
-    print("{}".format(k))
+k = 0
+for l in range(len(sys.argv) - 1):
+    k += int(sys.argv[l + 1])
+print("{}".format(k))
