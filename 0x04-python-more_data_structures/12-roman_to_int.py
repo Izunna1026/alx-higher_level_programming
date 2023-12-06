ea4dd1bc012feeddcc7db60a3a10f906b7d48426
Izunna1026 <done4emma@yahoo.com>
2023-12-06 19:50:49 +0000

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance (roman_string, str) or roman_string is None:
+        return 0
+    else:
+        roman_figure= {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        res = 0
+        prev = 0
+
+        for number in reversed(roman_string):
+            value = roman_figure.get(number, 0)
+            if value < prev:
+                res -= value
+            else:
+                res += value
+            prev = value
+        return res
