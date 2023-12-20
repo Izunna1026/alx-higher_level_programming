459c259f5e206faacbe1cb02ab50de71d053c4c5
izunna1026 <done4emma@yahoo.com>
2023-12-20 05:50:00 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
     @property
     def size(self):
         """to set the current size of the square"""
-        reutn(self.__size)
+        return(self.__size)
 
     @size.setter
     def size(self, value):
