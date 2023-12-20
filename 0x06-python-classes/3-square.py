ccc2ee7e0f068884dc5a41325e0509a209705ce3
izunna1026 <done4emma@yahoo.com>
2023-12-19 20:24:11 -0800

    task3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,6 @@
 """To define a class square"""
 
 
-
 class Square:
     """Representing a square"""
 
@@ -14,7 +13,7 @@ class Square:
             size (int): the size of the new square
         """
         if not isinstance(size, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
