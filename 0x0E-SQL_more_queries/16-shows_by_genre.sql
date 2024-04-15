ae0d4a38d99aa1d40b25d67097824cd22721f354
ichibueke <done4emma@yahoo.com>
2024-04-15 12:52:47 +0000

    task100

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- import database of tv_shows
+-- write a script that lists all shows
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title, tv_genres.name;
